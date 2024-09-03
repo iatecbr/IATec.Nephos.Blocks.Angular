5ad0467f0b6fa094f58247a8e3282cd14c0e9577
Elvys Marcos <elvys.marcos@iatec.com>
2024-09-03 14:49:08 -0300

    fix: Fix translation

--- projects/stage/src/app/app.config.ts
+++ projects/stage/src/app/app.config.ts
@@ -1,14 +1,23 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient } from '@angular/common/http';
+import { provideTransloco } from '@jsverse/transloco';
 
 export const appConfig: ApplicationConfig = {
     providers: [
         provideAnimations(),
         provideRouter(routes),
-        provideHttpClient()
+        provideHttpClient(),
+        provideTransloco({
+            config: {
+                availableLangs: ['pt-BR', 'en-US', 'es-ES'],
+                defaultLang: localStorage.getItem('lang') ?? 'en-US',
+                reRenderOnLangChange: true,
+                prodMode: !isDevMode(),
+            },
+        })
     ]
 };
