165a29223f2f8c7ba6e76df8836f7eebf2747b3c
Elvys Marcos <elvys.marcos@iatec.com>
2024-09-10 17:14:24 -0300

    feat: Improves empty page

--- projects/iatec/nephos-pages/src/lib/empty/empty-page.component.ts
+++ projects/iatec/nephos-pages/src/lib/empty/empty-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'nph-pages-empty',
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
     templateUrl: './empty-page.component.html'
 })
 export class EmptyPageComponent {
-
+    @Input() contentStyle: 'card' | 'none' = 'card';
 }
