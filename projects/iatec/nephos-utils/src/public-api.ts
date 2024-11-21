d97b6b176376ce4ffc47f2324fc10471716d4100
Elvys Marcos Ribeiro Bispo <elvys.marcos@gmail.com>
2024-11-21 18:58:36 -0300

    feat: Create sidebar helper in progress...

--- projects/iatec/nephos-utils/src/public-api.ts
+++ projects/iatec/nephos-utils/src/public-api.ts
@@ -3,11 +3,11 @@
  */
 
 export * from './lib/constants';
-export * from './lib/helpers/forms';
 export * from './lib/models';
 export * from './lib/services';
 export * from './lib/tests';
 export * from './lib/types';
 export * from './lib/validators';
+export * from './lib/helpers/forms';
 export * from './lib/helpers/summary';
 export * from './lib/helpers/sidebar';
