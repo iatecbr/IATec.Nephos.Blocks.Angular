41e63104b8cdbbf5028d201e4b15b5db643314d9
Elvys Marcos Ribeiro Bispo <elvys.marcos@gmail.com>
2024-12-25 12:19:29 -0300

    feat:Add new translation keys ****

--- projects/iatec/nephos-utils/src/lib/constants/translation.keys.ts
+++ projects/iatec/nephos-utils/src/lib/constants/translation.keys.ts
@@ -19,5 +19,26 @@ export const GeneralTranslationKeys = {
     preposition: {
         to: 'general.preposition.to',
         of: 'general.preposition.of'
+    },
+    message: {
+        error: {
+            forbidden: 'general.message.error.forbidden',
+            internalServerError: 'general.message.error.internalServerError',
+            network: 'general.message.error.network',
+            notFound: 'general.message.error.notFound',
+            ops: 'general.message.error.ops',
+            unauthorized: 'general.message.error.unauthorized',
+            validationError: 'general.message.error.validationError'
+        },
+        info: {
+            nothingHereYet: 'general.message.info.nothing-here-yet'
+        },
+        success: {
+            everythingOk: 'general.message.success.everything-ok',
+            toggleStatus: 'general.message.success.toggle-status'
+        },
+        warning: {
+            wantToProceed: 'general.message.warning.want-to-proceed'
+        }
     }
 }
