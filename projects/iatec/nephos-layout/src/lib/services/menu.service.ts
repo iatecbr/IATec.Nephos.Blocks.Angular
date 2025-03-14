7ce6be26975dadc352fc0a01ae1648b774150e23
Elvys Marcos Ribeiro Bispo <elvys.marcos@gmail.com>
2025-03-14 19:23:41 -0300

    fix: Update menu item visibility checks and adjust tailwind CSS import path

--- projects/iatec/nephos-layout/src/lib/services/menu.service.ts
+++ projects/iatec/nephos-layout/src/lib/services/menu.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
-import { MenuChangeEventModel } from '../models';
 import { MenuItem } from 'primeng/api';
 
 @Injectable({
@@ -8,23 +7,9 @@ import { MenuItem } from 'primeng/api';
 })
 export class MenuService {
 
-    private _menuSource = new Subject<MenuChangeEventModel>();
-    private _resetSource = new Subject();
     private _menus: Subject<MenuItem[]> = new Subject<MenuItem[]>();
 
-
-    menuSource$ = this._menuSource.asObservable();
-    resetSource$ = this._resetSource.asObservable();
     menus$ = this._menus.asObservable();
-
-
-    onMenuStateChange(event: MenuChangeEventModel) {
-        this._menuSource.next(event);
-    }
-
-    reset() {
-        this._resetSource.next(true);
-    }
 
     set menus(menus: MenuItem[]) {
         this._menus.next(menus);
