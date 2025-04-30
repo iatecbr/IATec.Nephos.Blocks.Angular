c394ba7d333258924673648fbcfb8ec724b80fd9
Elvys Marcos Ribeiro Bispo <elvys.marcos@gmail.com>
2025-04-30 08:14:24 -0300

    refactor(style): streamline TailwindCSS integration
    Simplified TailwindCSS imports and configurations across project files. Removed old compatibility styles and outdated configurations, ensuring alignment with the latest TailwindCSS standards. This change leads to cleaner and more maintainable CSS setup.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,22 @@
-module.exports = {
-    content: [
-        "./node_modules/@iatec/nephos-layout/**/*.{html,js}",
-        "./node_modules/@iatec/nephos-pages/**/*.{html,ts}",
-    ],
-}
+/** @type {import('tailwindcss').Config} */
+import PrimeUI from 'tailwindcss-primeui';
+
+export default {
+  darkMode: ['selector', '[class*="app-dark"]'],
+  content: [
+    './projects/stage/src/**/*.{js,ts,html}',
+    './projects/stage/src/assets/**/*.json',
+    '@iatec/nephos-layoutb/**/*.{js,ts,html}',
+    '@iatec/nephos-layout/**/*.{js,ts,html}',
+  ],
+  plugins: [PrimeUI],
+  theme: {
+    screens: {
+      sm: '576px',
+      md: '768px',
+      lg: '992px',
+      xl: '1200px',
+      '2xl': '1920px'
+    }
+  }
+};
