b6d5d18645408673b080f316c8b707168ff62a2c
elvys.marcos <elvys.marcos@iatec.com>
2025-07-18 16:53:14 -0300

    style(html): correct closing tag style
    Adjusted the closing tag for the link element to enhance HTML code consistency and
    compliance with XHTML standards.
    
    refactor(errors): standardize error message keys
    
    Refactored error message keys to include a consistent prefix, improving readability
    and maintainability of error handling across the application.

--- projects/iatec/nephos-utils/src/lib/constants/error.messages.ts
+++ projects/iatec/nephos-utils/src/lib/constants/error.messages.ts
@@ -1,9 +1,9 @@
 export const ErrorMessages = {
     ops: 'general.message.error.ops',
-    networkError: 'general.error.network',
-    unauthorized: 'general.error.unauthorized',
-    forbidden: 'general.error.forbidden',
-    notFound: 'general.error.not-found',
-    internalServerError: 'general.error.internal-server-error',
-    validationError: 'general.error.validation-error',
+    networkError: 'general.message.error.network',
+    unauthorized: 'general.message.error.unauthorized',
+    forbidden: 'general.message.error.forbidden',
+    notFound: 'general.message.error.not-found',
+    internalServerError: 'general.message.error.internal-server-error',
+    validationError: 'general.message.error.validation-error',
 };
