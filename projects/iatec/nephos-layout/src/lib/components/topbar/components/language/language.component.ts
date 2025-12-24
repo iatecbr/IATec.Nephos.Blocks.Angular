4713c0b46d1ec78f56788b081177b053d98a0aab
elvys.marcos <elvys.marcos@iatec.com>
2025-12-24 12:18:33 -0300

    chore(layout): update language component and version
    Updated the language component to use a configurable flag base URL and added LowerCasePipe.
    Changed flag URLs to use lowercase country codes and updated package version to 8.1.1.
    This improves flexibility for different flag asset sources and maintains consistency.

--- projects/iatec/nephos-layout/src/lib/components/topbar/components/language/language.component.ts
+++ projects/iatec/nephos-layout/src/lib/components/topbar/components/language/language.component.ts
@@ -1,26 +1,28 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
-import {PrimeTemplate} from 'primeng/api';
-import {FormsModule} from '@angular/forms';
-import { UpperCasePipe } from '@angular/common';
-import {Skeleton} from 'primeng/skeleton';
-import {Select} from 'primeng/select';
-import {LanguageModel} from '../../../../models';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { PrimeTemplate } from 'primeng/api';
+import { FormsModule } from '@angular/forms';
+import { LowerCasePipe, UpperCasePipe } from '@angular/common';
+import { Skeleton } from 'primeng/skeleton';
+import { Select } from 'primeng/select';
+import { LanguageModel } from '../../../../models';
 
 @Component({
     selector: 'nph-layout-topbar-language',
     templateUrl: './language.component.html',
     imports: [
-    PrimeTemplate,
-    FormsModule,
-    UpperCasePipe,
-    Skeleton,
-    Select
-],
+        PrimeTemplate,
+        FormsModule,
+        Skeleton,
+        Select,
+        LowerCasePipe,
+        UpperCasePipe
+    ],
     styleUrl: './language.component.scss'
 })
 export class LanguageComponent {
     @Input() languages: LanguageModel[] = [];
     @Input() selectedLanguage: LanguageModel | undefined;
+    @Input() flagBaseUrl: string = 'https://assets-services.sdasystems.org/images/flags/1x1/';
     @Output() selectedLanguageChange: EventEmitter<LanguageModel | undefined> = new EventEmitter<LanguageModel | undefined>();
 
     onChangeSelectedLanguage(option: LanguageModel | undefined) {
